29c283e102cea8737f50e097dd7c4215b8ee4799
phanther1 <144461089+phanther1@users.noreply.github.com>
2024-01-10 10:15:13 -0500

    odp

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-//function to do smth
+const URL = `https://random-word.ryanrk.com/api/en/word/random`;
 
-//
+async function getData(URL) {
+    try {
+        const response = await fetch(URL);
+        if (response.status !== 200) {
+            throw new Error(response.statusText);
+        }
+        const grab = await response.json();
+        console.log(grab.data);
+
+    } catch (error) {
+        console.log("something broke", error);
+    }
+    }
+    getData(URL);
+
+    //function for user input to unscramble word
+
+    //display scrambled words
+
+    //function for limit of input per turn
+
+    //algorithm to see if letters match in strings
+
+    //array to store used words, prevent reuse
