97cc3a9ac19eaa279b265073d483cb95b45d48fd
Karl F <144461089+phanther1@users.noreply.github.com>
2024-01-27 14:36:38 -0500

    wee

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-/* const URL = `https://random-word.ryanrk.com/api/en/word/random`; */
+/* const URL = `https://random-word.ryanrk.com/api/en/word/random`;
+deciding whether to use api or array
 
-/* async function getData(URL) {
+async function getData(URL) {
     try {
         const response = await fetch(URL);
         if (response.status !== 200) {
@@ -50,7 +51,7 @@
            word: "chantress",
         },
         {
-           pword: "Phebe",
+           word: "phebe",
         },
         {
            word: "sweetland",
@@ -73,56 +74,61 @@
 
     ]
 
-    const find = arr[(Math.floor(Math.random() * arr.length))];
-   /*     console.log(find); */
+    const displayWords = document.querySelector(".words");
+    const wordSubmit = document.getElementById("#submitForm");
+
+    function startGame() {
+      const randomWord = arr[Math.floor(Math.random() * arr.length)];
+      const originalWord = randomWord.word;
+      const scrambledWord = scrambleWord(originalWord);
+      displayWords.textContent = `Scrambled word: ${scrambledWord}`;
+  
+      submitForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const results = document.getElementById("#userResponse").value.toLowerCase();
+        if (results === originalWord.toLowerCase()) {
+          alert("Correct! You unscrambled the word! Click to refresh!");
+          startGame();
+        } else {
+          alert("That's Wrong! Try again.");
+        }
+      });
+    }
+  
+    function scrambleWord(word) {
+      const letters = word.split('');
+      for (let i = letters.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [letters[i], letters[j]] = [letters[j], letters[i]];
+      }
+      return letters.join('');
+    }
+    startGame();
+
+    
+/*     const find = arr[(Math.floor(Math.random() * arr.length))];
+      console.log(find);
 
     function randomWord() {
       const find = arr[(Math.floor(Math.random() * arr.length))];
       console.log(find);
       console.log(find.split(''));
-/*         const scramble = find.split((a, b) => 0.5 - math.random());
-        for (i= arr.length - 1; i >0 ; i++) {
+        const scramble = find.split((a, b) => 0.5 - math.random());
+        for (i= arr.length - 1; i > 0 ; i--) {
          let a = [0]
          let b = scramble b
       
-         console.log(scramble(a,b)); */
+         console.log(scramble(a,b));
          document.querySelector("#words").insertAdjacentHTML(randomWord());
         }
     randomWord();
-
-    function compare() {
-      document.querySelector("#word-form").addEventListener("submit", function () {
-         if ( //user input = find.result
-
-          `<div>
-          <dialog class = "diabox">
-           <h3>${ "variable or smth for whether same or not" }</h3>
-          <dialog>
-          </div>`
-         );
-         else (
-          //display the not same variable
-          
-         );
-         });
-    }
-
-    function something {
-      //add 1 for each win, or just jump to reset
-      document.querySelector("diabox").innerHTML;
-
-      for()
-    }
-
-    
+    } */
 
 
-    //function for user input to unscramble word
+    //function for user input to unscramble word done
 
-    //display scrambled words
+    //display scrambled words done
 
-    //function for limit of input per turn
+    //function for limit of input per turn to be done :)
 
-    //algorithm to see if letters match in strings
-
-    //try again button, new word button
+    //algorithm to see if letters match in strings done
